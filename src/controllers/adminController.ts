b2ff759533e2b62d5bb7f93500fd7b99c0713a47
Omar Khaled <omarkhaledvii@gmail.com>
2020-03-26 18:29:15 +0200

    Added admin(Get all helpers, Approve helper, Delete user) Endpoints

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -13,100 +13,101 @@ import clientModel from '../models/Client';
 import userModel from '../models/User';
 
 class AdminController implements IController {
-    public path:string;
-    public router:express.IRouter;
-    
-    constructor(){
+    public path: string;
+    public router: express.IRouter;
+    constructor() {
         this.path = '/Admin';
         this.router = express.Router();
         this.router.use(authMiddleware);
         this.router.use(adminMiddleware)
         this.initializeRoutes();
     }
-    private initializeRoutes(){
-        this.router.get(`${this.path}/GetPendingHelpers`,this.getPendingHelpers)
-        this.router.get(`${this.path}/GetAllHelpers`,this.getAllHelpers)
-        this.router.get(`${this.path}/GetAllClients`,this.getAllClients)
-        this.router.get(`${this.path}/GetPhoto`,this.getPhoto)
+    private initializeRoutes() {
+        this.router.get(`${this.path}/GetPendingHelpers`, this.getPendingHelpers)
+        this.router.get(`${this.path}/GetAllHelpers`, this.getAllHelpers)
+        this.router.get(`${this.path}/GetAllClients`, this.getAllClients)
+        this.router.get(`${this.path}/GetPhoto`, this.getPhoto)
         ////////////////////////////////////////////////////////////////////////////////////////////////////
-        this.router.post(`${this.path}/ApproveHelper`,this.approveHelper);
+        this.router.post(`${this.path}/ApproveHelper`, this.approveHelper);
         ////////////////////////////////////////////////////////////////////////////////////////////////////
-        this.router.delete(`${this.path}/DeleteUser`,this.deleteUser)
+        this.router.delete(`${this.path}/DeleteUser`, this.deleteUser)
     }
-    private getPhoto =  async (request:IRequestWithUser,response:express.Response,next:express.NextFunction) =>{
+    private getPhoto = async (request: IRequestWithUser, response: express.Response, next: express.NextFunction) => {
         const path = request.query.path;
         response.sendFile(path, (err) => {
             if (err) {
                 next(err);
             }
             else {
-              console.log("sent");
+                console.log("sent");
             }
-          });
+        });
     }
-    private getPendingHelpers =  async (request:IRequestWithUser,response:express.Response,next:express.NextFunction) =>{
-        await helperModel.find({isApproved:false},'-password -createdAt -updatedAt -__v' ,(err,helpers)=>{
-            if(err){
+    private getPendingHelpers = async (request: IRequestWithUser, response: express.Response, next: express.NextFunction) => {
+        await helperModel.find({ isApproved: false }, '-password -createdAt -updatedAt -__v', (err, helpers) => {
+            if (err) {
                 response.status(400).send(err);
             }
-            else{
+            else {
                 response.status(200).send(helpers);
             }
         })
     }
-    private deleteUser =  async (request:IRequestWithUser,response:express.Response,next:express.NextFunction) =>{
-        await userModel.deleteOne({_id:request.query.id},(err)=>{
-            if(err){
-                next(err)
+    private deleteUser = async (request: IRequestWithUser, response: express.Response, next: express.NextFunction) => {
+        const userID = request.body._id;
+        await userModel.findOneAndRemove({ _id: userID }, (err) => {
+            if (err) {
+
+                response.status(400).send('Faild to Delete User');
             }
-            else{
+            else {
                 response.status(200).send("User Deleted Successfully");
             }
         })
     }
-    private getAllClients =  async (request:IRequestWithUser,response:express.Response,next:express.NextFunction) =>{
-        await clientModel.find({},'-password -createdAt -updatedAt -__v' ,(err,clients:IClient[])=>{
-            if(err){
+    private getAllClients = async (request: IRequestWithUser, response: express.Response, next: express.NextFunction) => {
+        await clientModel.find({}, '-password -createdAt -updatedAt -__v', (err, clients: IClient[]) => {
+            if (err) {
                 response.status(400).send(err);
             }
-            else{
+            else {
                 let returnedClients = [];
-                for(let i=0;i<clients.length;i++){
-                  const newClient = clients[i].toObject();
-                  newClient.picture 
+                for (let i = 0; i < clients.length; i++) {
+                    const newClient = clients[i].toObject();
+                    newClient.picture
                 }
                 response.status(200).send(clients);
             }
         })
     }
-    private getAllHelpers =  async (request:IRequestWithUser,response:express.Response,next:express.NextFunction) =>{
-        await helperModel.find({},'-password -createdAt -updatedAt -__v' ,(err,helpers)=>{
-            if(err){
+    private getAllHelpers = async (request: IRequestWithUser, response: express.Response, next: express.NextFunction) => {
+        await helperModel.find({}, '-password -createdAt -updatedAt -__v', (err, helpers) => {
+            if (err) {
                 response.status(400).send(err);
             }
-            else{
+            else {
                 response.status(200).send(helpers);
             }
         })
     }
-    private approveHelper =  async (request:IRequestWithUser,response:express.Response,next:express.NextFunction) =>{
-      const helperID = request.body.id;
-      const helper = await helperModel.findById(helperID);
-      if(helper){
-          helper.isApproved = true;
-          await helper.save((err)=>{
-            if(err){
-                next(err);
-            }  
-            else{
-                response.status(200).send("Helper Approved");
-            }
+    private approveHelper = async (request: IRequestWithUser, response: express.Response, next: express.NextFunction) => {
+        const helperID = request.body._id;
+        const helper = await helperModel.findById(helperID);
+        if (helper) {
+            helper.isApproved = true;
+            await helper.save((err) => {
+                if (err) {
+                    next(err);
+                }
+                else {
+                    response.status(200).send("Helper Approved");
+                }
 
-          });
-      }
-      else{
-        response.status(400).send("Failed To Approve Helper");    
-      }
+            });
+        }
+        else {
+            response.status(400).send("Failed To Approve Helper");
+        }
     }
 }
 
